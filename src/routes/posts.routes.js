cf10b9b3f3ff64b1c45cb5d463c5b8a8cc5c295f
MarksASP95 <marcosuarezpetrillo@hotmail.com>
2019-11-06 15:41:14 -0400

    Added basic routes

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,6 +1,12 @@
 const { Router } = require('express')
 const router = Router();
+const { getPosts, getPost, createPost } = require('../controllers/post.controller')
 
+router.route('/')
+    .get(getPosts)
+    .post(createPost)
 
+router.route('/:id')
+    .get(getPost)
 
 module.exports = router;
