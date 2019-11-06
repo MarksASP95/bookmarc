f0e69c92ff05f5ec0b2131f2d202348cfd52874e
MarksASP95 <marcosuarezpetrillo@hotmail.com>
2019-11-06 15:41:32 -0400

    Added basic routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,6 +1,24 @@
 const { Router } = require('express')
+const { getUsers, createUser, getUser } = require('../controllers/user.controller');
+
 const router = Router();
 
+// getPosts (feed: por fecha descendente)
+// getPosts (por búsqueda)
+// getPost (por id)
+// createPost
+// editPost (igual que create)
+// deletePost
 
+// router.route('/')
+//     .get()
+//     .post()
+
+router.route('/')
+    .get(getUsers)
+    .post(createUser)
+
+router.route('/:id')
+    .get(getUser)
 
 module.exports = router;
