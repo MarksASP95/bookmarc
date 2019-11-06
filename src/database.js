3756bad6aceba0c126b129f5625a28f515de8acc
MarksASP95 <marcosuarezpetrillo@hotmail.com>
2019-11-06 15:39:38 -0400

    Added DB connection file

---
+++ src/database.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+const URI = process.env.MONGODB_URI
+
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log('DB is connected');
+})
