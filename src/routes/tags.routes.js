f85bc9a5b44ef378846d5223cf2cc166db4afa9c
MarksASP95 <marcosuarezpetrillo@hotmail.com>
2019-11-07 14:10:22 -0400

    Added routes

--- src/routes/tags.routes.js
+++ src/routes/tags.routes.js
@@ -1,6 +1,11 @@
 const { Router } = require('express')
+const { getTags, getTag } = require('../controllers/tag.controller')
 const router = Router();
 
+router.route('/')
+    .get(getTags)
 
+router.route('/:name')
+    .get(getTag)
 
 module.exports = router;
