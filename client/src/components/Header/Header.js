b9ad3dd1a7c661d805c718e31e337cd25c6543fb
MarksASP95 <marcosuarezpetrillo@hotmail.com>
2019-11-11 09:24:47 -0400

    Added Header component

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+import './Header.scss'
+
+export default class Header extends Component {
+
+    render() {
+        return (
+            <header className="header-component">
+                <div className="container">
+                    <div className="home-link">
+                        
+                    </div>
+                </div>
+            </header>
+        )
+    }
+}
