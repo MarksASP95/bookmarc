412b7e01e1cb53563418c97a3a54a85812f807ea
MarksASP95 <marcosuarezpetrillo@hotmail.com>
2019-11-11 09:24:13 -0400

    Added React Router

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Header from './components/Header/Header'
+
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Router>
+        {/* <Route path="/" exact component={} /> */}
+      </Router>
     </div>
   );
 }
