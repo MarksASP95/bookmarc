e8083f7a73dbd4b4a09724b379a4d5594c96d50d
MarksASP95 <marcosuarezpetrillo@hotmail.com>
2019-11-13 11:19:44 -0400

    Added testing post data

---
+++ client/src/components/Feed/postList.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        title: "Make your first blog using NodeJS + ExpressJS + ReactJS",
+        author: "marcoasp",
+        date: new Date('2019/11/12'),
+        tags: ["javascript", "reactjs", "frameworks"],
+        num_likes: 23,
+        num_comments: 4,
+        read_minutes: 5
+    },
+    {
+        title: "Make your first blog using NodeJS + ExpressJS + ReactJS",
+        author: "marcoasp",
+        date: new Date('2019/11/12'),
+        tags: ["javascript", "reactjs", "frameworks"],
+        num_likes: 23,
+        num_comments: 4,
+        read_minutes: 5
+    },
+    {
+        title: "Make your first blog using NodeJS + ExpressJS + ReactJS",
+        author: "marcoasp",
+        date: new Date('2019/11/12'),
+        tags: ["javascript", "reactjs", "frameworks"],
+        num_likes: 23,
+        num_comments: 4,
+        read_minutes: 5
+    },
+    {
+        title: "Make your first blog using NodeJS + ExpressJS + ReactJS",
+        author: "marcoasp",
+        date: new Date('2019/11/12'),
+        tags: ["javascript", "reactjs", "frameworks"],
+        num_likes: 23,
+        num_comments: 4,
+        read_minutes: 5
+    }
+]
