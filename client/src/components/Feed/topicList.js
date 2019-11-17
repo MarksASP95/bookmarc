d757268975eab06f7dd7b28a225f45d9095604a3
MarksASP95 <marcosuarezpetrillo@hotmail.com>
2019-11-17 18:37:53 -0400

    Added test topic list

---
+++ client/src/components/Feed/topicList.js
@@ -0,0 +1,12 @@
+export default [
+    "javascript",
+    "productivity",
+    "python",
+    "reactjs",
+    "machine_learning",
+    "angular",
+    "ios",
+    "vuejs",
+    "android",
+    "best_practices"
+]
