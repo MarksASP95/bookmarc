96ad08908f1aaed5af73470e757b2a1bb6db433c
MarksASP95 <marcosuarezpetrillo@hotmail.com>
2019-11-17 18:38:16 -0400

    Main component testing

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlus, faBell, faUser, faHeart, faComment } from '@fortawesome/free-solid-svg-icons'
 
 import Header from './components/Header/Header'
+import PostCard from './components/Feed/PostCard/PostCard'
+import Container from './components/Container/Container';
+import Display from './components/Display/Display';
+import Feed from './components/Feed/Feed';
 
 function App() {
+
+  library.add(faPlus, faBell, faUser, faHeart, faComment)
+
   return (
     <div className="App">
-      <Header />
       <Router>
-        {/* <Route path="/" exact component={} /> */}
+        <Header />
+        {/* <Route path="/" exact component={Feed} /> */}
+        <Container>
+          <Display>
+            <Feed />
+          </Display>
+        </Container>
       </Router>
     </div>
   );
