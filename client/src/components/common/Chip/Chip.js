a368c96dcf30e5eac69e88a7925cc7635a4a5353
MarksASP95 <marcosuarezpetrillo@hotmail.com>
2019-11-19 23:43:56 -0400

    Added fade animation

--- client/src/components/common/Chip/Chip.js
+++ client/src/components/common/Chip/Chip.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade'
 
 import './Chip.scss'
+import '../../../constants/styles/common/general.scss'
 
 export default function Chip(props) {
     return (
-        <div onClick={props.onClick} className="chip-component">
-            <span>{props.text}</span>
-        </div>
+        <Fade duration={100}>
+            <div onClick={props.onClick} className="chip-component">
+                <span className="unselectable">{props.text}</span>
+            </div>
+        </Fade>
     )
 }
