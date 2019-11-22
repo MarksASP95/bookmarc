ab4051dbe082df85cabe84d709cbb01999af85b5
MarksASP95 <marcosuarezpetrillo@hotmail.com>
2019-11-21 23:19:14 -0400

    Added Loading Bar component

---
+++ client/src/components/common/LoadingBar/LoadingBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './LoadingBar.scss'
+
+export default function LoadingBar() {
+    return (
+        <div className="loading-bar-component">
+              <div class="loading-bar">
+                <div class="loading-bar-value"></div>
+            </div>
+        </div>
+    )
+}
