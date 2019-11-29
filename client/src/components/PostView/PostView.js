eac9948455916fd0450ec5711474a7d2c260c784
MarksASP95 <marcosuarezpetrillo@hotmail.com>
2019-11-29 15:31:51 -0400

    Added Post View component

---
+++ client/src/components/PostView/PostView.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PostCard from '../Feed/PostCard/PostCard'
+
+export default function PostView() {
+    return (
+        <div className='post-view-component'>
+            <PostCard  />
+        </div>
+    )
+}
