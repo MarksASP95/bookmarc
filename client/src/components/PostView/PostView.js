40863d0181a833f6383c767ad916f0e38f397666
MarksASP95 <marcosuarezpetrillo@hotmail.com>
2019-12-01 19:57:01 -0400

    Displays parsed content

--- client/src/components/PostView/PostView.js
+++ client/src/components/PostView/PostView.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react' 
 
 import axios from 'axios'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import bbCodeParser from 'js-bbcode-parser'
+import parse from 'html-react-parser'
 
 import PostCard from '../Feed/PostCard/PostCard'
 
@@ -9,8 +12,9 @@ import './PostView.scss'
 export default function PostView(props) {
 
     const [post, setPost] = useState(null);
+    const [likeButtonIO, setLikeButtonIO] = useState(false)
 
-    const postId = '5dd5d7384b243a1d88bc16fe'
+    const postId = props.match.params.post_id
 
     useEffect(() => {
         const getPost = async () => {
@@ -23,7 +27,13 @@ export default function PostView(props) {
         getPost()
     }, [])
 
+    const getPostContent = () => {
+        return parse(bbCodeParser.parse(post.content))
+    }
 
+    const toggleLike = () => {
+        setLikeButtonIO(!likeButtonIO)
+    }
 
     
     return (
@@ -41,7 +51,13 @@ export default function PostView(props) {
                     read_minutes={Math.ceil(post.content.split(" ").length / 220)}
                 /> 
                 <div className="post-content">
-                    {post.content}
+                    {getPostContent()}
+                </div>
+                <div className="feedback-container aqua-black-border shadow-side">
+                    <p className="feedback-instruction">Did you like this post?</p>
+                    <div className="heart-button" onClick={toggleLike}>
+                        <span className={likeButtonIO ? 'on' : 'off'}><FontAwesomeIcon icon="heart"></FontAwesomeIcon></span>
+                    </div>
                 </div>
             </React.Fragment>
             :
