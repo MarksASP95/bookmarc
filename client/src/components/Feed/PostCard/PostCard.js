a02f0716e96b1cec0f3f03d736ab3e245b67bd48
MarksASP95 <marcosuarezpetrillo@hotmail.com>
2019-11-30 21:40:56 -0400

    Refactored for >2 card types

--- client/src/components/Feed/PostCard/PostCard.js
+++ client/src/components/Feed/PostCard/PostCard.js
@@ -8,6 +8,7 @@ import userPicture from '../../../assets/images/test/marcoasp.PNG'
 
 import './PostCardMain.scss'
 import './PostCardRegular.scss'
+import './PostCardView.scss'
 import '../../../constants/styles/common/border.scss'
 
 const PostCard = (props) => {
@@ -21,36 +22,67 @@ const PostCard = (props) => {
         tags = `${tags} #${tag}`
     })
 
+    const getPostMetaData = () => {
+
+        const bannerDiv = <div className="post-banner" style={{backgroundImage:`url(${postBanner})`}}></div>
+        const userPictureDiv = <div className="user-picture" style={{backgroundImage:`url(${userPicture})`}}></div>
+        const postMetaMainP = 
+            <p className="post-meta-main">
+                {`${props.author} - ${months[props.date.getMonth()]} ${props.date.getDate()} (${format(props.date)})`}
+            </p>
+        const postFeedbackContainer = 
+            <React.Fragment>
+                <div className="likes feedback">
+                    <FontAwesomeIcon icon="heart" />
+                    <span>{props.num_likes}</span>
+                </div>
+                <div className="comments feedback">
+                    <FontAwesomeIcon icon="comment"/>
+                    <span>{props.num_comments}</span>
+                </div>
+            </React.Fragment>
+        const byUserSpan = <span className="by">by {props.author}</span>
+
+        switch(props.type) {
+            case 'main':
+                return {
+                    bannerElement: bannerDiv,
+                    userPictureElement: userPictureDiv,
+                    metaMainElement: postMetaMainP,
+                    feedbackElement: postFeedbackContainer,
+                    byUserElement: null
+                }
+            case 'regular':
+                return {
+                    bannerElement: null,
+                    userPictureElement: userPictureDiv,
+                    metaMainElement: null,
+                    feedbackElement: null,
+                    byUserElement: byUserSpan
+                }
+            case 'view':
+                return {
+                    bannerElement: bannerDiv,
+                    userPictureElement: null,
+                    metaMainElement: null,
+                    feedbackElement: null,
+                    byUserElement: null
+                }
+        }
+    }
+
+    const postMetaData = getPostMetaData()
+
     return (
         <div className={`postcard-component ${props.type} aqua-black-border shadow-side`}>
-            {props.main ? <div className="post-banner" style={{backgroundImage:`url(${postBanner})`}}></div> : null}
+            {postMetaData.bannerElement}
             <div className="post-meta">
                 <h2 className="post-title">{props.title}</h2>
-                <div className="user-picture" style={{backgroundImage:`url(${userPicture})`}}></div>
-                {
-                    props.main ? 
-                    <p className="post-meta-main">
-                        {`${props.author} - ${months[props.date.getMonth()]} ${props.date.getDate()} (${format(props.date)})`}
-                    </p>
-                    : 
-                    null
-                }
+                {postMetaData.userPictureElement}
+                {postMetaData.metaMainElement}
                 <p className="tags">{tags}</p>
-                {
-                    props.main ? 
-                    <React.Fragment>
-                        <div className="likes feedback">
-                            <FontAwesomeIcon icon="heart" />
-                            <span>{props.num_likes}</span>
-                        </div>
-                        <div className="comments feedback">
-                            <FontAwesomeIcon icon="comment"/>
-                            <span>{props.num_comments}</span>
-                        </div>
-                    </React.Fragment>
-                    :
-                    <span className="by">by {props.author}</span>
-                }
+                {postMetaData.feedbackElement}
+                {postMetaData.byUserElement}
                 <span className="read-minutes">{`${props.read_minutes} minute read`}</span>
             </div>
         </div>
