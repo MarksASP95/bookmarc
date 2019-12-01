c71714a93aab934db8e8b465b4997f1a9cd53b24
MarksASP95 <marcosuarezpetrillo@hotmail.com>
2019-11-30 21:43:13 -0400

    Added Post View component

--- client/src/components/PostView/PostView.js
+++ client/src/components/PostView/PostView.js
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react' 
+
+import axios from 'axios'
+
 import PostCard from '../Feed/PostCard/PostCard'
 
-export default function PostView() {
+import './PostView.scss'
+
+export default function PostView(props) {
+
+    const [post, setPost] = useState(null);
+
+    const postId = '5dd44565a03fc40fc400eb9c'
+
+    useEffect(() => {
+        const getPost = async () => {
+
+            const result = await axios.get(`http://localhost:4000/api/posts/${postId}`)
+
+            setPost(result.data)
+        }
+
+        getPost()
+    }, [])
+
+
+
+    
     return (
         <div className='post-view-component'>
-            <PostCard  />
+            {post ?
+            <PostCard
+                type='main'
+                title={post.title}
+                author={post.author.username}m
+                date={new Date(post.date)}
+                tags={post.tags}
+                num_likes={post.num_likes}
+                num_comments={post.comments.length}
+                read_minutes={Math.ceil(post.content.split(" ").length / 220)}
+            /> 
+            // JSON.stringify(post.data)
+            :
+            null
+            }
+
         </div>
     )
 }
