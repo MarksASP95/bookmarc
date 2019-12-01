d4ac2192c4fa56045bdf8c26fcddc00090b8802d
MarksASP95 <marcosuarezpetrillo@hotmail.com>
2019-12-01 19:55:32 -0400

    Click on post card redirects to it

--- client/src/components/Feed/Posts/Posts.js
+++ client/src/components/Feed/Posts/Posts.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 
+import { Redirect } from 'react-router-dom'
+
 import PostCard from './../PostCard/PostCard';
 
 import './Posts.scss'
 
+
 export default class Posts extends Component {
+
+    state = {
+        redirect: false,
+        postId: null
+    }
+
+
+
     render() {
         return (
             <div className="posts-component">
@@ -18,8 +29,10 @@ export default class Posts extends Component {
                         num_likes={post.num_likes}
                         num_comments={post.comments.length}
                         read_minutes={Math.ceil(post.content.split(" ").length / 220)}
+                        onClick={() => this.setState({redirect: true, postId: post._id})}
                     />
                 ))}
+                {this.state.redirect ? <Redirect to={`/post/${this.state.postId}`} /> : null}
             </div>
         )
     }
